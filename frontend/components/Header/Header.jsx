d8b03d88343d58563d87fb52085f4ec5daf94d6a
wilbertbw <wilbertbwibowo@gmail.com>
2025-08-05 13:58:01 -0400

    added a header and updated styling

---
+++ frontend/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <div className="header">
+    <h2>Realtime Chat</h2>
+  </div>
+);
+
+export default Header;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import { connect, sendMsg } from './api/index.js'
+import Header from '../components/Header/Header.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <button onClick={this.send}>Hit</button>
       </div>
     );
