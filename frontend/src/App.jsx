0801c41eb501cdd06ce445350e993ab24d374496
wilbertbw <wilbertbwibowo@gmail.com>
2025-08-05 12:04:32 -0400

    added code in App.jsx to start the websocket connection and send a message when button is clicked

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Component } from 'react'
 import './App.css'
+import { connect, sendMsg } from './api/index.js'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.send}>Hit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
 export default App
