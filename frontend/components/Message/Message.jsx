19b85604610b9a6c969350f23bbc865a731eb331
wilbertbw <wilbertbwibowo@gmail.com>
2025-08-05 17:00:18 -0400

    added a Message component that displays the messages

---
+++ frontend/components/Message/Message.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Message.css';
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    let temp = JSON.parse(this.props.message);
+    this.state = {
+      key: this.props.key,
+      message: temp
+    };
+  }
+
+  render() {
+    return <div className="Message">{this.state.message.body}</div>
+  }
+}
+
+export default Message;
